113576715a4935aafded0205157b8c13c4b50e27
bmcculley <box76135@gmail.com>
2019-07-11 01:20:58 -0400

    hello, world

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify, \
+                    redirect, Response, url_for, abort
+import requests
+
+# flask app setup
+app = Flask(__name__)
+app.secret_key = "update_me"
+
+hidden_address = "email@example.com"
+captcha_secret_key = "SECRET"
+recaptcha_url = "https://www.recaptcha.net/recaptcha/api/siteverify"
+
+def verify(response, client_ip):
+	payload = {"secret":captcha_secret_key,
+		"response":response,
+		"remoteip":client_ip}
+	#"https://www.recaptcha.net/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip
+	r = requests.get(recaptcha_url, params=payload)
+	r_data = r.json()
+	return r_data["success"]
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("home.html")
+
+@app.route("/validate", methods=["POST"])
+def validate():
+	data = None
+	client_ip = request.remote_addr
+	captcha_response = request.form['g-recaptcha-response']
+	if verify(captcha_response, client_ip):
+		data = {"status":True,
+			"msg":"Here's the email you were looking for",
+			"email":hidden_address}
+	else:
+		data = {"status":False,
+			"msg":"reCAPTCHA test failed."}
+	return render_template("validate.html", data=data)
+
+if __name__ == "__main__":
+	app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,26 @@
+Flask Mailhide
+==============
+
+Get your reCAPTCHA api from https://www.google.com/recaptcha
+
+# Flask Mailhide
+
+A replacement of Google's [Mailhide](https://www.google.com/recaptcha/mailhide/d) (deprecated due to the shutdown of reCAPTCHA v1) using [reCAPTCHA](https://developers.google.com/recaptcha/intro) v2. 
+
+
+## Usage
+
+1. [Sign up](http://www.google.com/recaptcha/admin) for your own reCAPTCHA key from Google. Both regular and invisible keys are supported.
+2. Replace `<site_key>` in `home.html` and `SECRET` in `app.py` with your own. 
+4. Include a link to your application in the web page where your masked email address is shown. 
+
+Example:
+
+```html
+<p><a href="https://example.com" onclick="window.open('https://example.com', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">foo&hellip;</a>@github.com (Click to reveal email address)</p>
+```
+
+
+## Notes
+
+The code uses `www.recaptcha.net` instead of `www.google.com` to enable access from places where Google is blocked. See the [FAQ](https://developers.google.com/recaptcha/docs/faq#can-i-use-recaptcha-globally) of reCAPTCHA for details on global access. 
